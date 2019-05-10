df88cbbc557b0fe0f44b825d0703068d254a9ec2
tylermcbrien <50494074+tylermcbrien@users.noreply.github.com>
2019-05-10 11:24:15 -0400

    Add files via upload

---
+++ Week 1 Work.py
@@ -0,0 +1,37 @@
+# Question 4
+radius = 4.85
+pie = 3.1215926
+print(3.1215926*4.85**2)
+
+# Question 8
+help(print)
+
+# Question 10
+help(pow)
+
+# Question 12
+import math
+math.sqrt(3)
+
+# Question 14
+present_value=124
+cash_flow=50
+discount_rate = cash_flow/present_value
+print(50/124)
+
+# Question 16
+variance=0.2**2
+days = 10
+daily_standard_deviation=(variance*days)
+print(10*v)
+
+# Question 18
+import re
+re.
+
+# Question 20
+mr = "mean return"
+mf = "mean risk-free rate"
+r = "risk"
+sharpe = ((mr-mf)/r)
+print(sharpe)
