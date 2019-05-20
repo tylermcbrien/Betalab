385726199db8d591c8f46329199e92fd143548ab
tylermcbrien <50494074+tylermcbrien@users.noreply.github.com>
2019-05-20 19:45:37 -0400

    Add files via upload

---
+++ Week 2 Work.py
@@ -0,0 +1,22 @@
+"Question 6"
+import numpy as np
+np.set_printoptions
+x=np.zeros((3,3))
+print(x)
+
+"Question 8"
+from scipy.stats import skewtest
+stats.skewtest ([1,2,3,4,5,6])
+
+"Question 11"
+import scipy as sp
+ret=sp.array([2,3,4])
+"Arithemtic Mean"
+sp.mean(ret)
+"Geometric Mean"
+pow(sp.prod(ret+1),1./len(ret))-1
+
+"Question 12"
+a=np.array([0.8,0.7,0.5,0.6,0.8])
+from scipy import stats
+stats.zscore(a)
