0eaea3b44a554d7657a245e1baf13e089d61ec05
tylermcbrien <50494074+tylermcbrien@users.noreply.github.com>
2019-05-26 22:13:54 -0400

    Add files via upload

---
+++ Week 3 Questions.py
@@ -0,0 +1,12 @@
+Python 3.7.3 (v3.7.3:ef4ec6ed12, Mar 25 2019, 21:26:53) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> import scipy as sp
+>>> sp.fv(.0025,25,0,5000)
+-5322.057184363162
+>>> import scipy as sp
+>>> sp.pv(.0541/2,20,55,0,1)
+-863.7816463604273
+>>> import scipy as sp
+>>> sp.pmt(.0312,5,2400)
+-525.8478442817194
+>>> 
